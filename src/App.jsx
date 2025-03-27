f912447e280f3719f02e8b845fdd25dc60dc3f92
Irfan ansari <irfanking8215@gmai.com>
2025-03-27 17:17:27 +0530

    Added new features/updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 
 import Hero from './Components/Hero/Hero'
 import Navbar from './Components/Navbar/Navbar'
+import Programs from './Components/Programs/Programs'
+
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
-      <Hero/>
+      <Navbar />
+      <Hero />
+      <div className="contsiner">
+        <Programs />
+
+      </div>
 
     </div>
   )
